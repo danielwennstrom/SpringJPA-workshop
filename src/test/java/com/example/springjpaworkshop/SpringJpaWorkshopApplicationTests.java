4e02d4f4a45967d65107fd1200116e7b5f6b151f
danielwennstrom <daniel@wennstrom.io>
2025-04-09 09:32:18 +0200

    part 1

---
+++ src/test/java/com/example/springjpaworkshop/SpringJpaWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjpaworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
