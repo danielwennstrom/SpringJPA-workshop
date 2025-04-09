9a98abcf9cfe4430c58c7744773060d283265e14
danielwennstrom <daniel@wennstrom.io>
2025-04-09 10:08:04 +0200

    DetailsRepository setup

--- src/main/java/com/example/springjpaworkshop/repository/DetailsRepository.java
+++ src/main/java/com/example/springjpaworkshop/repository/DetailsRepository.java
@@ -1,7 +1,12 @@
 package com.example.springjpaworkshop.repository;
 
+import com.example.springjpaworkshop.entity.Details;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class DetailsRepository {
+public interface DetailsRepository extends CrudRepository<Details, Integer> {
+    Details findByEmail(String email);
+    Details findByNameContains(String name);
+    Details findByNameIgnoreCase(String name);
 }
