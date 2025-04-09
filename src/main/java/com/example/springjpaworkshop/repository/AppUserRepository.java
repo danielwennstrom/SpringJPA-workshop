3c09febc3ed57f2d78165c931061144dac0c7a66
danielwennstrom <daniel@wennstrom.io>
2025-04-09 10:06:29 +0200

    AppUserRepository setup

--- src/main/java/com/example/springjpaworkshop/repository/AppUserRepository.java
+++ src/main/java/com/example/springjpaworkshop/repository/AppUserRepository.java
@@ -1,7 +1,16 @@
 package com.example.springjpaworkshop.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import com.example.springjpaworkshop.entity.AppUser;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
-public class AppUserRepository {
+public interface AppUserRepository extends CrudRepository<AppUser, Integer> {
+    AppUser findByUsername(String username);
+    List<AppUser> findByRegDateBetween(LocalDate regDateAfter, LocalDate regDateBefore);
+    List<AppUser> findByDetailsId(Integer id);
+    AppUser findByDetailsEmailIgnoreCase(String email);
 }
