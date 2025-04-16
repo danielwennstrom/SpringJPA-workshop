6c59a97c88bedf38f3f02c51a18dbefb80ab5fb1
Daniel Wennström <daniel@wennstrom.io>
2025-04-16 10:12:45 +0200

    Unit tests for BookRepositoryTest

---
+++ src/test/java/com/example/springjpaworkshop/repository/BookRepositoryTest.java
@@ -0,0 +1,73 @@
+package com.example.springjpaworkshop.repository;
+
+import com.example.springjpaworkshop.entity.Book;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.util.Assert;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class BookRepositoryTest {
+    @Autowired
+    BookRepository bookRepository;
+    
+    Book book;
+    Book book2;
+    
+    @BeforeEach
+    public void setUp() {
+        book = new Book();
+        book.setTitle("Book 1");
+        book.setMaxLoanDays(30);
+        
+        book2 = new Book();
+        book2.setTitle("Book 2");
+        book2.setMaxLoanDays(60);
+        
+        bookRepository.save(book);
+        bookRepository.save(book2);
+    }
+    
+    @Test
+    public void testFindAllBooks() {
+        Iterable<Book> books = bookRepository.findAll();
+        for (Book b : books) {
+            System.out.println(b.getTitle());
+        }
+    }
+    
+    @Test
+    public void testUpdateBook() {
+        book.setTitle("Updated Book 1");
+        bookRepository.save(book);
+        
+        book = bookRepository.findByTitleContains("Updated Book 1").getFirst();
+        assertEquals("Updated Book 1", book.getTitle());
+    }
+    
+    @Test
+    public void testDeleteBook() {
+        bookRepository.delete(book);
+        Optional<Book> getBook = bookRepository.findById(1);
+        assertTrue(getBook.isEmpty());
+    }
+    
+    @Test
+    public void testFindByTitle() {
+        Book foundBook = bookRepository.findByTitleContains("Book 1").getFirst();
+        assertNotNull(foundBook);
+        assertEquals("Book 1", foundBook.getTitle());
+    }
+    
+    @Test
+    public void testMaxLoanDaysLessThan() {
+        List<Book> books = bookRepository.findByMaxLoanDaysLessThan(35);
+        assertEquals(1, books.size());
+    }
+}
