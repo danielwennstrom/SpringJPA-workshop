3acf465b8f78073a3ee82171990ec8f1f69e2026
Daniel Wennström <daniel@wennstrom.io>
2025-04-22 09:24:36 +0200

    BookLoan list to AppUser, add/remove BookLoan methods

--- src/main/java/com/example/springjpaworkshop/entity/AppUser.java
+++ src/main/java/com/example/springjpaworkshop/entity/AppUser.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -26,4 +28,16 @@ public class AppUser {
     @OneToOne
     @JoinColumn(name = "details_id")
     private Details userDetails;
+    @OneToMany(mappedBy = "borrower", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BookLoan> bookLoans = new ArrayList<>();
+    
+    public void addBookLoan(BookLoan bookLoan) {
+        this.bookLoans.add(bookLoan);
+        bookLoan.setBorrower(this);
+    }
+    
+    public void removeBookLoan(BookLoan bookLoan) {
+        this.bookLoans.remove(bookLoan);
+        bookLoan.setBorrower(null);
+    }
 }
